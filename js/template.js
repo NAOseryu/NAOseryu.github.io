c22b517df5299d35f8dfc2be93d89185ab588566
NAOseryu <masah3325@gmail.com>
2020-03-22 18:07:17 +0900

    ユーザ名が取れないため外だし

--- js/template.js
+++ js/template.js
@@ -1,11 +1,11 @@
-$(function(){
-  var poolData = {
-    UserPoolId : 'ap-northeast-1_5ynbUsboz',
-    ClientId : '4so6sevjpqoqmmhh3ki24r11fn'
-  };
-  // ユーザ名取得
-  var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
-  const cognitoUser = userPool.getCurrentUser();  // 現在のユーザー
+var poolData = {
+  UserPoolId : 'ap-northeast-1_5ynbUsboz',
+  ClientId : '4so6sevjpqoqmmhh3ki24r11fn'
+};
+// ユーザ名取得
+var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+const cognitoUser = userPool.getCurrentUser();  // 現在のユーザー
 
+$(function(){
   $("#header").load("../wacca/include/header.html");
 });
