16122e0c6d563cc81f1dc35cf3114c13d5b6f10f
NAOseryu <masah3325@gmail.com>
2020-03-23 23:27:06 +0900

    ブックマークレット修正

--- js/bookmarklet.js
+++ js/bookmarklet.js
@@ -1,58 +1,82 @@
 // てんあさん(@chirping_crow)のWACCAスコア集計ツールを(勝手に)参考にしてます。ありがとうございます。
-var getData = function() {
-  let results = [];
+const dispStatus = function() {
+  return new Promise((resolve,reject) => {
+    const toolBg = $('<div>').addClass('toolBg').attr('style','position: fixed; top: 0; z-index: 10; width: 100%; height:100%; background-color: rgba(0,0,0,0.6);');
+    const infoArea = $('<div>').addClass('infoArea').attr('style', 'width: 100%; padding: 5px; background-color: #f82374; color: #fcfcfc;');
+    const infoSpan = $('<span>').addClass('infoSpan').text('スコアを取得します。');
+    infoArea.append(infoSpan);
+    toolBg.append(infoArea);
+    $('body').append(toolBg);
+    resolve("f1 ==> f2");
+  })
+}
 
-  let tmpScore, tmpResult;
-  let inputs = $('input[name="musicId"]');
-  let nodes = $('[class*="muuri-item"]');
+const getData = function(passVal) {
+  return new Promise((resolve,reject) => {
+    setTimeout(function(){
+      let results = [];
 
-  for (let i = 1; i <= nodes.length; i++) {
-    input = inputs[i - 1];
-    musicId = $(input).val();
-    tmpLevel = nodes[i - 1].querySelectorAll('.playdata__score-list__song-info__lv');
-    tmpScore = nodes[i - 1].querySelectorAll('.playdata__score-list__song-info__score');
+      let tmpScore, tmpResult;
+      let inputs = $('input[name="musicId"]');
+      let nodes = $('[class*="muuri-item"]');
 
-    console.log(musicId);
-    // 各曲のスコアページを取得
-    $.ajax({
-      type: 'POST',
-      url: 'https://wacca.marv-games.jp/web/music/detail',
-      data: 'musicId=' + musicId,
-      async: false,
-    })
-    .done(function(data) {
-      tmpPlayCount = $(data).find('[class*="song-info__top__play-count"]');
-      console.log(tmpPlayCount);
+      for (let i = 1; i <= nodes.length; i++) {
+        setTimeout(function(){
+          input = inputs[i - 1];
+          musicId = $(input).val();
+          tmpLevel = nodes[i - 1].querySelectorAll('.playdata__score-list__song-info__lv');
+          tmpScore = nodes[i - 1].querySelectorAll('.playdata__score-list__song-info__score');
 
-      tmpResult = {
-        musicId: musicId,
-        title: nodes[i - 1].querySelector('.playdata__score-list__song-info__name').innerText,
-        n_level: tmpLevel[0].innerText.replace(/\s+/g, "").replace("NORMAL", ""),
-        h_level: tmpLevel[1].innerText.replace(/\s+/g, "").replace("HARD", ""),
-        e_level: tmpLevel[2].innerText.replace(/\s+/g, "").replace("EXPERT", ""),
-        n_score: tmpScore[0].innerText.replace(/\s+/g, "").replace("SCORE", ""),
-        h_score: tmpScore[1].innerText.replace(/\s+/g, "").replace("SCORE", ""),
-        e_score: tmpScore[2].innerText.replace(/\s+/g, "").replace("SCORE", ""),
-        n_playCount: tmpPlayCount[0].innerText.replace(/\s+/g, "").replace("プレイ回数：", "").replace("回", ""),
-        h_playCount: tmpPlayCount[1].innerText.replace(/\s+/g, "").replace("プレイ回数：", "").replace("回", ""),
-        e_playCount: tmpPlayCount[2].innerText.replace(/\s+/g, "").replace("プレイ回数：", "").replace("回", ""),
+          console.log(musicId);
+          // 各曲のスコアページを取得
+          $.ajax({
+            type: 'POST',
+            url: 'https://wacca.marv-games.jp/web/music/detail',
+            data: 'musicId=' + musicId,
+            async: false,
+          })
+          .done(function(data) {
+            tmpPlayCount = $(data).find('[class*="song-info__top__play-count"]');
+            console.log(tmpPlayCount);
+
+            tmpResult = {
+              musicId: musicId,
+              title: nodes[i - 1].querySelector('.playdata__score-list__song-info__name').innerText,
+              n_level: tmpLevel[0].innerText.replace(/\s+/g, "").replace("NORMAL", ""),
+              h_level: tmpLevel[1].innerText.replace(/\s+/g, "").replace("HARD", ""),
+              e_level: tmpLevel[2].innerText.replace(/\s+/g, "").replace("EXPERT", ""),
+              n_score: tmpScore[0].innerText.replace(/\s+/g, "").replace("SCORE", ""),
+              h_score: tmpScore[1].innerText.replace(/\s+/g, "").replace("SCORE", ""),
+              e_score: tmpScore[2].innerText.replace(/\s+/g, "").replace("SCORE", ""),
+              n_playCount: tmpPlayCount[0].innerText.replace(/\s+/g, "").replace("プレイ回数：", "").replace("回", ""),
+              h_playCount: tmpPlayCount[1].innerText.replace(/\s+/g, "").replace("プレイ回数：", "").replace("回", ""),
+              e_playCount: tmpPlayCount[2].innerText.replace(/\s+/g, "").replace("プレイ回数：", "").replace("回", ""),
+            }
+            results.push(tmpResult)
+          });
+          $('.infoSpan').text(i + '/' + nodes.length + ' 曲完了');
+        }, 200)
       }
-
-      results.push(tmpResult)
-    })
-  }
-  downloadText(results)
+      resolve(results);
+    }, 100);
+  })
 }
 
 // テキストで出力
-var downloadText = function(results) {
-  let resultJson = JSON.stringify(results);
-  const bom = new Uint8Array([0xEF, 0xBB, 0xBF]);
-  let blob = new Blob([bom, resultJson], { "type" : "text/plain" });
-  let link = document.createElement('a');
-  link.href = URL.createObjectURL(blob);
-  link.download = 'wacca_playdata.txt';
-  link.click();
+const downloadText = function(results) {
+  return new Promise((resolve,reject) => {
+    setTimeout(function(){
+      let resultJson = JSON.stringify(results);
+      const bom = new Uint8Array([0xEF, 0xBB, 0xBF]);
+      let blob = new Blob([bom, resultJson], { "type" : "text/plain" });
+      let link = document.createElement('a');
+      link.href = URL.createObjectURL(blob);
+      link.download = 'wacca_playdata.txt';
+      link.click();
+      $('.toolBg').remove();
+      resolve("f1 ==> f2");
+    }, 1000);
+  })
 }
 
 // コピーが上手くいかないのでそのうちやる。。。
@@ -86,7 +110,9 @@ try {
     throw new Error("表示しているページが違います。プレイデータの楽曲スコアのページを開いて再度試してください。");
   }
 
-  results = getData();
+  dispStatus()
+  .then(getData)
+  .then(downloadText)
 
 } catch (e) {
   alert(e);
