5e766b21787acd349ad19fbdf244b7423ced9654
NAOseryu <masah3325@gmail.com>
2024-04-20 00:21:28 +0900

    難易度表のブックマークレット追加

---
+++ ddr/difficulty_list_bookmarklet.js
@@ -0,0 +1,22 @@
+// 「javascript:(function(url){s=document.createElement('script');s.src=url;document.body.appendChild(s);})('https://naoseryu.github.io/ddr/difficulty_list_bookmarklet.js')」
+// をブックマークに登録して実行すること
+for (chartKey of Object.keys(difficultyList).concat(missingChartsChartKeys)) {
+	let score = indexedAllScores[chartKey];
+
+	if (score) {
+		scoreText = document.createElement('p');
+		scoreText.style.fontSize='0.5em';
+		scoreText.style.margin='0px 0px 0px 0px'
+		scoreText.textContent = score.score
+
+		if (score.lamp == '0') {
+			scoreText.style.color = '#404040';
+		} else if (score.score >= 990000) {
+			scoreText.style.color = '#FFFF00';
+		} else if (score.score < 790000) {
+			scoreText.style.color = '#00FFFF';
+		}
+
+		$('#div-jacket-' + chartKey.replace('/', '-')).append(scoreText);
+	}
+}
