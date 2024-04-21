e34d64d1a52f3bb43470ed9aebb3f4dd921ab2ae
NAOseryu <masah3325@gmail.com>
2024-04-21 17:15:41 +0900

    更新日時追加

--- ddr/difficulty_list_bookmarklet.js
+++ ddr/difficulty_list_bookmarklet.js
@@ -5,8 +5,9 @@ for (chartKey of Object.keys(difficultyList).concat(missingChartsChartKeys)) {
 
 	if (score) {
 		scoreText = document.createElement('p');
-		scoreText.style.fontSize='0.5em';
+		scoreText.style.fontSize='20px';
 		scoreText.style.margin='0px 0px 0px 0px'
+		scoreText.style.paddingRight='5px'
 		scoreText.textContent = score.score
 
 		if (score.lamp == '0') {
@@ -18,5 +19,14 @@ for (chartKey of Object.keys(difficultyList).concat(missingChartsChartKeys)) {
 		}
 
 		$('#div-jacket-' + chartKey.replace('/', '-')).append(scoreText);
+
+		scoreTime = document.createElement('p');
+		scoreTime.style.fontSize='20px';
+		scoreTime.style.margin='0px 0px 0px 0px'
+		scoreTime.style.paddingRight='5px'
+		let updateTime = new Date(score.score_time * 1000);
+		scoreTime.textContent = updateTime.toLocaleDateString()
+
+		$('#div-jacket-' + chartKey.replace('/', '-')).append(scoreTime);
 	}
 }
